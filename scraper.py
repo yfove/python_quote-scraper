3ecd30087726c0ced83d345491dfba011376ab38
Curtis <curtis.ch.chat@gmail.com>
2023-01-11 16:53:59 -0700

    scrape current page

--- scraper.py
+++ scraper.py
@@ -1,3 +1,26 @@
-import random
+from bs4 import BeautifulSoup
+import requests
+from time import sleep
 
-def
+
+url = "http://quotes.toscrape.com"
+response = requests.get(url).text
+soup = BeautifulSoup(response, 'html.parser')
+quotes = soup.select('.quote')
+arr = []
+
+for quote in quotes:
+    quote_text = quote.select('.text')[0].get_text()
+    # quote text is located inside a span with class of text
+    author = quote.select('author')[0].get_text()
+    # author name in a small tag with class author
+    arr.append({
+        'quote':quote_text,
+        "author":author
+    })
+
+print(arr)
+
+
+print(my_arr)
+
